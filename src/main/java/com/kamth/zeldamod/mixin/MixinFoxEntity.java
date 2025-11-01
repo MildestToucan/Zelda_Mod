18ac50266c2398ebf9f32e8f8f1736ac6db79790
MildToucan <introvertpale@gmail.com>
2025-11-01 06:37:15 +0100

    Switch MixinFoxEntity injector to @ModifyReturnValue, clean up formatting, rename injector handler This should allow for smoother chaining with other mods' modifications to the target. The name also now better reflects what the mixin does

--- src/main/java/com/kamth/zeldamod/mixin/MixinFoxEntity.java
+++ src/main/java/com/kamth/zeldamod/mixin/MixinFoxEntity.java
@@ -1,38 +1,27 @@
 package com.kamth.zeldamod.mixin;
 
 import com.kamth.zeldamod.item.ZeldaItems;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.UUID;
 
 
-
-
-
 @Mixin(Fox.class)
-public abstract class MixinFoxEntity extends Animal
-{
-    // This constructor is fake and never used
-    protected MixinFoxEntity()
-    {
+abstract class MixinFoxEntity extends Animal {
+
+    protected MixinFoxEntity() {
         super(null,null);
     }
 
-    @Inject(method = "trusts", at = @At("HEAD"), cancellable = true)
-    private void Trustmode(UUID uuid, CallbackInfoReturnable<Boolean> ci)
-    {
-        Player player = level().getPlayerByUUID(uuid);
-
-        if (player != null && player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ZeldaItems.KEATON_MASK.get())
-        {
-            ci.setReturnValue(true);
-        }
+    @ModifyReturnValue(method = "trusts", at = @At("RETURN"))
+    private boolean trustWhenWearingKeatonMask(boolean original, UUID uuid) {
+        Player player = this.level().getPlayerByUUID(uuid);
+        return original || (player != null && player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ZeldaItems.KEATON_MASK.get());
     }
 }
